f40c4049705a4870f9908965ff2d71163617bd3a
nhuse <nathan.huse@yahoo.com>
2021-08-08 14:07:46 -0500

    commit tree 220106b8e29e053df1a10ed9b4a1b675c71378f1

--- control_flow.rb
+++ control_flow.rb
@@ -1,16 +1,42 @@
 def admin_login(username, password)
+  if ((username == "admin" || username == "ADMIN") && password == "12345")
+    return "Access granted"
+  end
+
+  "Access denied"
   # your code here
 end
 
 def hows_the_weather(temperature)
-  # your code here
+  if temperature < 40
+    return "It's brisk out there!"
+  elsif temperature.between?(40, 65)
+    return "It's a little chilly out there!"
+  elsif temperature > 85
+    return "It's too dang hot out there!"
+  end
+
+  "It's perfect out there!"
 end
 
 def fizzbuzz(num)
-  # your code here
+  if num%5 == 0 && num%3 == 0
+    return "FizzBuzz"
+  elsif num%3 == 0
+    return "Fizz"
+  elsif num%5 == 0
+    return "Buzz"
+  end
+
+  num
 end
 
 def calculator(operation, num1, num2)
-  # your code here
-end
+  if(operation == "+" || operation == "-" || operation == "*" || operation == "/")
+    return num1.send(operation, num2)
+  end
 
+  puts "Invalid operation!"
+  nil
+
+end
